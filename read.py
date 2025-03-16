3782aa05a5189bb38ae7c4f12b4aa433473dda06
Wace <wacebbbbbb@gmail.com>
2025-03-16 19:18:11 +0800

    Add files via upload

---
+++ read.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+datas = np.load('./label_mind.npz')
+print(datas.files)
+
+data_dict = {key: datas[key] for key in datas.files}
+
+m = datas['labels']
+print(m)
+# data_dict['labels'] = np.array([1, 2, 3, 4, 5, 6])#换行间隔
+# print(m)
+
+# np.savetxt('./M.txt', data_dict['labels'], delimiter=" ")    #保存为txt
+# np.savetxt('./M.csv', data_dict['labels'], delimiter=",")
+#
+# np.savez('./label_mind.npz', **data_dict)
